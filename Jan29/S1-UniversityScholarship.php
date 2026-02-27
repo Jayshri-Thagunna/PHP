113b14ba661af5f964655295d1d276272a36fee8
Jayshri-Thagunna <thagunnajayshri02@gmail.com>
2026-02-27 15:11:54 +0530

    update

---
+++ Jan29/S1-UniversityScholarship.php
@@ -0,0 +1,10 @@
+<?php
+    $cgpa = 6.67;
+    $income = 10000;
+
+    if($cgpa >= 8.0 && $income < 300000) {
+        echo "You are eligible";
+    } else {
+        echo "You are not eligible";
+    }
+?>
---
+++ Jan29/S4-VotingEligibility.php
@@ -0,0 +1,10 @@
+<?php
+    $age = 20;
+    $citizenship = true;
+
+    if($age >= 18 && $citizenship) {
+        echo "You are eligible to vote!";
+    } else {
+        echo "You are not eligible to vote! ";
+    }
+?>
