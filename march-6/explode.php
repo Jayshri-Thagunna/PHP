1fd8af1ebdf447343aaf1eb7c009693f36a3cf17
Jayshri-Thagunna <thagunnajayshri02@gmail.com>
2026-03-06 15:21:17 +0530

    implode and explode in array

---
+++ march-6/explode.php
@@ -0,0 +1,8 @@
+<?php
+//explode = string to array
+
+    $colour = 'red,blue,white,black';
+    print_r($colour);
+    $colour_arr = explode(",",$colour);
+    print_r($colour_arr)
+?>
---
+++ march-6/implode.php
@@ -0,0 +1,7 @@
+<?php
+//implode = array to string
+    $words_arr = array('Hie', 'bye', 'jay', 'shri');
+    print_r($words_arr);
+    $sentence_string = implode(" ",$words_arr);
+    print_r($sentence_string);
+?>
